88f3b975db83a6c770075406cb610bd172758dcb
Taz <taseen156@gmail.com>
2023-07-21 14:28:32 +0100

    First Task

---
+++ src/qa/practise/sky/Runner.java
@@ -0,0 +1,14 @@
+package qa.practise.sky;
+
+public class Runner {
+
+
+    public static void main(String[] args) {
+        String firstName = "Siam";
+        String lastName = "Islam";
+        String name = firstName +" "+ lastName;
+        System.out.println(name);
+    }
+
+
+}
